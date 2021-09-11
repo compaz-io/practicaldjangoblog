52d9bb9fd8c5998349a02ebbbf6c3d2ff161f0ab
Schweigs <mjschweiger@gmail.com>
2021-09-11 16:53:13 +0100

    add blog url structure and dummy views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def starting_page(request):
+    return HttpResponse('homepage')
+
+def posts(request):
+    return HttpResponse('Posts')
+
+def post_detail(request):
+    return HttpResponse('Post')
